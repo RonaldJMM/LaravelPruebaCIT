cc8b0930998be2dc7d7d0614525caaa49c6ef0be
Ronald Moreno <morenoronald7@gmail.com>
2020-05-10 19:59:57 -0500

    Cambio de imagen general

--- LaravelPruebaCIT/resources/views/content/content.blade.php
+++ LaravelPruebaCIT/resources/views/content/content.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="col-lg-8">@yield('contentPaginaPrincipal')</div>
         <div class="col-lg-8">@yield('contentUsuario')</div>
+        <div class="col-lg-4" style="align-content: center"><img src="{{ url('images/aplication/iconoProject.png') }}" style="display:block; " height="95%" width="100%"></div>
     </div>
 
     
