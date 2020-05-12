99d48f56c04defb809a78d9cff7252d928845783
Dairo Beltran <daiydan99@hotmail.com>
2020-05-11 20:54:39 -0500

    rutas

--- LaravelPruebaCIT/app/Http/Controllers/Auth/ResetPasswordController.php
+++ LaravelPruebaCIT/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/usuario/inicio';
 
     /**
      * Create a new controller instance.
